e285778b817ba3c12b1b61368ea7bb5c347b2c48
李欣 <lixin@lixin-computer.com>
2017-04-25 14:22:57 +0800

    macOS Version

---
+++ macOS Version/Cryption/Cryption/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Cryption
+//
+//  Created by 李欣 on 2017/4/22.
+//  Copyright © 2017年 李欣. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+@property IBOutlet NSTextField *text1;
+@property IBOutlet NSButton *button1;
+
+@end
+
---
+++ macOS Version/Cryption/Cryption/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Cryption
+//
+//  Created by 李欣 on 2017/4/22.
+//  Copyright © 2017年 李欣. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
